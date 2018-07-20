c403ba062d32bfdb6cd96b15d5ea0350da61c1fc
Bing <zhoubing05@meituan.com>
2018-07-20 17:31:16 +0800

    update .gitignore

--- web3j-app/codegen/build/resources/test/truffle/MetaCoin/ConvertLib.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.2;
-
-library ConvertLib{
-	function convert(uint amount,uint conversionRate) pure public returns (uint convertedAmount)
-	{
-		return amount * conversionRate;
-	}
-}
